06fa8ad0a2652c8e2a606cd2e342d3cf73560dd1
Kira <musngi.kier@gmail.com>
2017-12-10 11:17:47 +0800

    Revised GMSTest.py as general for other use.

--- GSMTest.py
+++ GSMTest.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python
+
 import serial
 from curses import ascii
 import time
 
-recipient = "+639272548944"
-message = "Sapnu puas"
+recipient = "+639123456789""
+message = "Hello World!"
 modem = serial.Serial('/dev/serial0', 115200, timeout=1)
 
 try:
     if modem.isOpen():
        modem.close()
-modem.open()
-modem.isOpen()
+    
+    modem.open()
+    modem.isOpen()
 
-modem.write("AT+CMGF=1\r")
-time.sleep(1)
-print modem.readline()
+    modem.write("AT+CMGF=1\r")
+    time.sleep(1)
+    print modem.readline()
 
-modem.write("AT+CSQ\r")
-time.sleep(1)
-print modem.readline()
+    modem.write("AT+CSQ\r")
+    time.sleep(3)
+    print modem.readline()
 
-modem.write('AT+CMGS="' + recipient.encode() + '"\r')
+    modem.write('AT+CMGS="' + recipient.encode() + '\r')
+    time.sleep(1)
+    modem.write(message.encode() + "\r")
+    time.sleep(1)
+    modem.write(ascii.ctrl('z'))
+    time.sleep(2)
 
-modem.write(message.encode() + "\r")
-modem.write(ascii.ctrl('z'))
-
-time.sleep(2)
-print modem.readlines()
+    print "Message Sent!"
 
 finally:
     modem.close()
